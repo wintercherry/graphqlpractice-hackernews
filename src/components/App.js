500976aec72f4ef174b8a74371322602902759d9
Michel Boto <michel.boto@gmail.com>
2022-04-23 17:03:36 +0200

    added Header and link navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
 import logo from '../logo.svg';
 import '../styles/App.css';
+import Header from './Header';
 import CreateLink from './CreateLink';
 import LinkList from './LinkList';
+import { Route, Routes } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
-    <CreateLink/>
-//    <LinkList/>
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Routes>
+          <Route path="/" element={<LinkList/>} />
+          <Route
+            path="/create"
+            element={<CreateLink/>}
+          />
+        </Routes>
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import { useNavigate } from 'react-router-dom';
 import { useMutation, gql } from '@apollo/client';
 
 const CREATE_LINK_MUTATION = gql`
@@ -17,56 +17,59 @@ const CREATE_LINK_MUTATION = gql`
 `;
 
 const CreateLink = () => {
-  const [formState, setFormState] = useState({
-    description: '',
-    url: ''
-  });
-
-  const [createLink] = useMutation(CREATE_LINK_MUTATION, {
-    variables: {
-      description: formState.description,
-      url: formState.url
-    }
+    const [formState, setFormState] = useState({
+        description: '',
+        url: ''
     });
 
-  return (
-    <div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          createLink();
-        }}
-      >
-        <div className="flex flex-column mt3">
-          <input
-            className="mb2"
-            value={formState.description}
-            onChange={(e) =>
-              setFormState({
-                ...formState,
-                description: e.target.value
-              })
-            }
-            type="text"
-            placeholder="A description for the link"
-          />
-          <input
-            className="mb2"
-            value={formState.url}
-            onChange={(e) =>
-              setFormState({
-                ...formState,
-                url: e.target.value
-              })
-            }
-            type="text"
-            placeholder="The URL for the link"
-          />
+    const navigate = useNavigate();
+
+    const [createLink] = useMutation(CREATE_LINK_MUTATION, {
+        variables: {
+            description: formState.description,
+            url: formState.url
+        },
+        onCompleted: () => navigate('/')
+    });
+
+    return (
+        <div>
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    createLink();
+                }}
+            >
+                <div className="flex flex-column mt3">
+                    <input
+                        className="mb2"
+                        value={formState.description}
+                        onChange={(e) =>
+                            setFormState({
+                                ...formState,
+                                description: e.target.value
+                            })
+                        }
+                        type="text"
+                        placeholder="A description for the link"
+                    />
+                    <input
+                        className="mb2"
+                        value={formState.url}
+                        onChange={(e) =>
+                            setFormState({
+                                ...formState,
+                                url: e.target.value
+                            })
+                        }
+                        type="text"
+                        placeholder="The URL for the link"
+                    />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
         </div>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default CreateLink;
