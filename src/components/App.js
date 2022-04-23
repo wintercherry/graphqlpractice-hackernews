7ba718b3f46c28c4e349314d411c02c3668ab33a
Michel Boto <michel.boto@gmail.com>
2022-04-23 15:53:35 +0200

    added create link functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import logo from '../logo.svg';
 import '../styles/App.css';
+import CreateLink from './CreateLink';
 import LinkList from './LinkList';
 
 function App() {
   return (
-    <LinkList/>
+    <CreateLink/>
+//    <LinkList/>
   );
 }
 
